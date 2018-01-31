32f52f3d22aad2326e6f1d147603f7c0b92a8f96
PurpleNinjaLink <jawhittom@gmail.com>
2018-01-31 13:08:00 -0600

    Functional Update
    It properly works and updates values now.

--- Assets/Scripts/FormulaResult.cs
+++ Assets/Scripts/FormulaResult.cs
@@ -19,29 +19,57 @@ public class FormulaResult : MonoBehaviour
     bool phyRes;
 
     [SerializeField]
-    InputField attackField;
+    RectTransform attackField;
     [SerializeField]
-    InputField speedField;
+    RectTransform speedField;
     [SerializeField]
-    InputField defenseField;
+    RectTransform defenseField;
     [SerializeField]
-    InputField healthField;
+    RectTransform healthField;
     [SerializeField]
     Text result;
 
 	// Use this for initialization
 	void Start () 
 	{
-        attackField.textComponent.text = atk.ToString();
-        speedField.textComponent.text = spd.ToString();
-        defenseField.textComponent.text = def.ToString();
-        healthField.textComponent.text = hp.ToString();
+        attackField.Find("Placeholder").GetComponent<Text>().text = atk.ToString();
+        speedField.Find("Placeholder").GetComponent<Text>().text = spd.ToString();
+        defenseField.Find("Placeholder").GetComponent<Text>().text = def.ToString();
+        healthField.Find("Placeholder").GetComponent<Text>().text = hp.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        result.text = string.Format("{0} - {1} = {2} damage.\nHealth remaining: {3}", atk, def, atk - def, hp - atk + def);
+        UpdateStats();
+        if (atk >= def)
+        {
+            if (hp - atk + def >= 0)
+                result.text = string.Format("{0} - {1} = {2} damage.\nHealth remaining: {3}", atk, def, atk - def, hp - atk + def);
+            else
+                result.text = string.Format("{0} - {1} = {2} damage.\nHealth remaining: 0", atk, def, atk - def);
+        }
+        else
+            result.text = string.Format("{0} - {1} = 0 damage.\nHealth remaining: {2}", atk, def, hp);
+    }
 
+    void UpdateStats()
+    {
+        if (attackField.Find("Text").GetComponent<Text>().text != "")
+            int.TryParse(attackField.Find("Text").GetComponent<Text>().text, out atk);
+        else
+            int.TryParse(attackField.Find("Placeholder").GetComponent<Text>().text, out atk);
+        if (speedField.Find("Text").GetComponent<Text>().text != "")
+            int.TryParse(speedField.Find("Text").GetComponent<Text>().text, out spd);
+        else
+            int.TryParse(speedField.Find("Placeholder").GetComponent<Text>().text, out spd);
+        if (defenseField.Find("Text").GetComponent<Text>().text != "")
+            int.TryParse(defenseField.Find("Text").GetComponent<Text>().text, out def);
+        else
+            int.TryParse(defenseField.Find("Placeholder").GetComponent<Text>().text, out def);
+        if (healthField.Find("Text").GetComponent<Text>().text != "")
+            int.TryParse(healthField.Find("Text").GetComponent<Text>().text, out hp);
+        else
+            int.TryParse(healthField.Find("Placeholder").GetComponent<Text>().text, out hp);
     }
 }
