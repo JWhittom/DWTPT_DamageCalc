d76f8a6058e11c51a7fd3db08afeb7855bba757a
Alexandra Fredrich <Alexandra.Fredrich@iam.local>
2018-01-31 17:26:38 -0600

    Meeting Update
    Functionality and appearance improved, calculations modified

--- Assets/Scripts/FormulaResult.cs
+++ Assets/Scripts/FormulaResult.cs
@@ -8,15 +8,13 @@ public class FormulaResult : MonoBehaviour
     [SerializeField]
     int atk = 10;
     [SerializeField]
-    int spd = 5;
+    int spd = 1;
     [SerializeField]
     int def = 7;
     [SerializeField]
     int hp = 20;
     [SerializeField]
-    bool magRes;
-    [SerializeField]
-    bool phyRes;
+    bool res;
 
     [SerializeField]
     RectTransform attackField;
@@ -29,7 +27,8 @@ public class FormulaResult : MonoBehaviour
     [SerializeField]
     Text result;
 
-	// Use this for initialization
+	private int damage;
+
 	void Start () 
 	{
         attackField.Find("Placeholder").GetComponent<Text>().text = atk.ToString();
@@ -37,17 +36,20 @@ public class FormulaResult : MonoBehaviour
         defenseField.Find("Placeholder").GetComponent<Text>().text = def.ToString();
         healthField.Find("Placeholder").GetComponent<Text>().text = hp.ToString();
     }
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
         UpdateStats();
-        if (atk >= def)
+		if (res)
+			damage = atk - Mathf.RoundToInt ((def * 1.15f) + .1f);
+		else
+			damage = atk - def;
+		if (damage > 0)
         {
-            if (hp - atk + def >= 0)
-                result.text = string.Format("{0} - {1} = {2} damage.\nHealth remaining: {3}", atk, def, atk - def, hp - atk + def);
+            if (hp - damage * spd >= 0)
+				result.text = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage.\nHealth remaining: {5}", atk, def, damage, spd, spd * damage, hp - (damage * spd));
             else
-                result.text = string.Format("{0} - {1} = {2} damage.\nHealth remaining: 0", atk, def, atk - def);
+				result.text = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage.\nHealth remaining: 0", atk, def, damage, spd, spd * damage);
         }
         else
             result.text = string.Format("{0} - {1} = 0 damage.\nHealth remaining: {2}", atk, def, hp);
@@ -59,8 +61,12 @@ public class FormulaResult : MonoBehaviour
             int.TryParse(attackField.Find("Text").GetComponent<Text>().text, out atk);
         else
             int.TryParse(attackField.Find("Placeholder").GetComponent<Text>().text, out atk);
-        if (speedField.Find("Text").GetComponent<Text>().text != "")
-            int.TryParse(speedField.Find("Text").GetComponent<Text>().text, out spd);
+		if (speedField.Find ("Text").GetComponent<Text> ().text != "") 
+		{
+			int.TryParse (speedField.Find ("Text").GetComponent<Text> ().text, out spd);
+			spd = Mathf.Min (3, spd);
+			spd = Mathf.Max (1, spd);
+		}
         else
             int.TryParse(speedField.Find("Placeholder").GetComponent<Text>().text, out spd);
         if (defenseField.Find("Text").GetComponent<Text>().text != "")
