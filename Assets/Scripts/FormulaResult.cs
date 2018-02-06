b4240d7bd343cb0f51d585656dbf6736aabe5b84
PurpleNinjaLink <jawhittom@gmail.com>
2018-02-06 14:37:55 -0600

    Two Players Implemented
    2-Player stats work. Need to refine UpdateStats to make them actually modifiable. Aside, code refactored to flow a bit better mostly kinda.

--- Assets/Scripts/FormulaResult.cs
+++ Assets/Scripts/FormulaResult.cs
@@ -6,16 +6,15 @@ using UnityEngine.UI;
 public class FormulaResult : MonoBehaviour 
 {
     [SerializeField]
-    int atk = 10;
+    Toggle resToggleOne;
     [SerializeField]
-    int spd = 1;
+    Toggle resToggleTwo;
     [SerializeField]
-    int def = 7;
-    [SerializeField]
-    int hp = 20;
-    [SerializeField]
-    bool res;
-
+    Text result;
+    [Tooltip("P1 Attack, P1 Defense, P1 Speed, P1 Health, P2 Attack, P2 Defense, P2 Speed, P2 Health")]
+    [SerializeField] List<RectTransform> statBoxes;
+    [SerializeField] List<int> numericStats;
+    /*
     [SerializeField]
     RectTransform attackField;
     [SerializeField]
@@ -24,67 +23,105 @@ public class FormulaResult : MonoBehaviour
     RectTransform defenseField;
     [SerializeField]
     RectTransform healthField;
-    [SerializeField]
-    Toggle resToggle;
-    [SerializeField]
-    Text result;
+    */
 
-    [SerializeField] List<RectTransform> statBoxes;
-    [SerializeField] List<int> numericStats;
 
 	private int damage;
+    int atk = 0;
+    int spd = 2;
+    int def = 5;
+    int hp = 7;
+    int enemySpd = 2;
+    int atkTimes = 1;
+    bool res;
+    bool resOne;
+    bool resTwo;
+    bool pTwoDead = false;
+    string resultString;
 
-	void Start () 
+    void Start () 
 	{
-        attackField.Find("Placeholder").GetComponent<Text>().text = atk.ToString();
-        speedField.Find("Placeholder").GetComponent<Text>().text = spd.ToString();
-        defenseField.Find("Placeholder").GetComponent<Text>().text = def.ToString();
-        healthField.Find("Placeholder").GetComponent<Text>().text = hp.ToString();
+        res = resTwo;
+        for (int i = 0; i < statBoxes.Count; i++)
+        {
+            statBoxes[i].Find("Placeholder").GetComponent<Text>().text = numericStats[i].ToString();
+        }
+
     }
 
 	void Update () 
 	{
         UpdateStats();
-        if (res)
+        for (int i = 1; i <= 2; i++)
         {
-            def = Mathf.RoundToInt((def * 1.15f) + .1f);
-            damage = atk - def;
-        }
-        else
-            damage = atk - def;
-		if (damage > 0)
-        {
-            if (hp - damage * spd >= 0)
-				result.text = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage.\nHealth remaining: {5}", atk, def, damage, spd, spd * damage, hp - (damage * spd));
+
+            if (res)
+            {
+                numericStats[def] = Mathf.RoundToInt((numericStats[def] * 1.15f) + .1f);
+                damage = numericStats[atk] - numericStats[def];
+            }
             else
-				result.text = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage.\nHealth remaining: 0", atk, def, damage, spd, spd * damage);
+                damage = numericStats[atk] - numericStats[def];
+            if (numericStats[spd] - numericStats[enemySpd] >= 5)
+                atkTimes = 2;
+            else
+                atkTimes = 1;
+		    if (damage > 0)
+            {
+                if (numericStats[hp] - damage * atkTimes >= 0)
+                    resultString = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage. Health remaining: {5}\n", 
+                        numericStats[atk], numericStats[def], damage, atkTimes, atkTimes * damage, numericStats[hp] - (damage * atkTimes));
+                else
+                {
+                    resultString = string.Format("{0} - {1} = {2} * {3} attack(s) = {4} damage. Health remaining: 0\n", 
+                        numericStats[atk], numericStats[def], damage, atkTimes, atkTimes * damage);
+                    if(i == 1)
+                        pTwoDead = true;
+                }
+            }
+            else
+                resultString = string.Format("{0} - {1} = 0 damage. Health remaining: {2}\n", 
+                    numericStats[atk], numericStats[def], numericStats[hp]);
+            if (i == 1 && !pTwoDead)
+            {
+                atk = 4;
+                spd = 6;
+                def = 1;
+                hp = 3;
+                enemySpd = 2;
+                res = resOne;
+                result.text = resultString;
+                result.text += "\nCounterattack:\n";
+            }
+            else
+            {
+                atk = 0;
+                spd = 2;
+                def = 5;
+                hp = 7;
+                enemySpd = 6;
+                res = resTwo;
+                if (!pTwoDead)
+                    result.text += resultString;
+                else
+                    result.text = resultString;
+            }
         }
-        else
-            result.text = string.Format("{0} - {1} = 0 damage.\nHealth remaining: {2}", atk, def, hp);
+        
     }
 
     void UpdateStats()
     {
-        res = resToggle.isOn;
-        if (attackField.Find("Text").GetComponent<Text>().text != "")
-            int.TryParse(attackField.Find("Text").GetComponent<Text>().text, out atk);
-        else
-            int.TryParse(attackField.Find("Placeholder").GetComponent<Text>().text, out atk);
-		if (speedField.Find ("Text").GetComponent<Text> ().text != "") 
-		{
-			int.TryParse (speedField.Find ("Text").GetComponent<Text> ().text, out spd);
-			spd = Mathf.Min (3, spd);
-			spd = Mathf.Max (1, spd);
-		}
-        else
-            int.TryParse(speedField.Find("Placeholder").GetComponent<Text>().text, out spd);
-        if (defenseField.Find("Text").GetComponent<Text>().text != "")
-            int.TryParse(defenseField.Find("Text").GetComponent<Text>().text, out def);
-        else
-            int.TryParse(defenseField.Find("Placeholder").GetComponent<Text>().text, out def);
-        if (healthField.Find("Text").GetComponent<Text>().text != "")
-            int.TryParse(healthField.Find("Text").GetComponent<Text>().text, out hp);
-        else
-            int.TryParse(healthField.Find("Placeholder").GetComponent<Text>().text, out hp);
+        resOne = resToggleOne.isOn;
+        resTwo = resToggleTwo.isOn;
+        for (int i = 0; i < statBoxes.Count; i++)
+        {
+            int val = 0;
+            if (statBoxes[i].Find("Text").GetComponent<Text>().text != "")
+                int.TryParse(statBoxes[i].Find("Text").GetComponent<Text>().text, out val);
+            else
+                int.TryParse(statBoxes[i].Find("Placeholder").GetComponent<Text>().text, out val);
+            numericStats[i] = val;
+        }
     }
 }
