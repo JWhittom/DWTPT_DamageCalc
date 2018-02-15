ec504d3afa0bf8be8318800f89e72db9c1b6927e
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2018-02-15 00:42:32 -0600

    Including a title screen and the new backgrounds
    Will work on button for results and change ambush stats tomorrow.

---
+++ Assets/Scripts/SceneMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMovement : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Default");
+    }
+
+	public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
